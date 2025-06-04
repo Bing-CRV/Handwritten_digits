202e43e5144c10e3b2220d62a0a562ebc32c303b
Bing <huanglibing311@gmail.com>
2025-06-04 21:14:58 +0800

    master

---
+++ model_test.py
@@ -0,0 +1,40 @@
+import torch
+import matplotlib.pyplot as plt
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from base_model import Net  # 导入Net模型
+from utils import ModelUtils
+# 使用自定义模型预测minst数据集(有标签)
+
+# 实例化并加载训练好的模型
+net = Net()
+model_path = './model/numbermodel_7_0.976_20241012203328.pth'
+net.load_state_dict(torch.load(model_path))
+net.eval()  # 设置模型为评估模式
+
+test_loader=ModelUtils.get_data_loader(False,50)
+
+# 获取一批测试数据
+images, labels = next(iter(test_loader))
+print("图片张数:",images.size())
+print(images.shape)
+# # 使用模型进行预测
+with torch.no_grad():
+    outputs = net(images.view(-1, 28*28))  # 将图片展平
+    predictions = torch.argmax(outputs, dim=1)
+    # 当前批的准确率
+    correct_predictions = (predictions == labels).sum().item()  # 正确预测的数量
+    total_samples = labels.size(0)  # 总样本数量
+    true_rate = correct_predictions / total_samples  # 当前批次的准确率
+print("预测:",predictions)
+print("实际:", labels)
+print("准确率:",true_rate)
+
+# # 创建子图
+for i in range(50):
+    plt.subplot(5,10,i+1)
+    plt.imshow(images[i].view(28,28),cmap="gray")
+    plt.title(f"{predictions[i].item()}")
+    plt.axis('off')
+plt.tight_layout()
+plt.show()
---
+++ model_train.py
@@ -0,0 +1,67 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import MNIST
+import matplotlib.pyplot as plt
+from datetime import datetime
+from base_model import Net  # 导入Net模型
+from utils import ModelUtils #导入模型工具类
+
+# 使用自定义神经网络基础模型通过minst训练集(有标签)训练模型,并通过测试集评估模型准确率\
+
+
+
+# 如果该文件作为主程序执行，则调用 main 函数
+if __name__ == "__main__":
+    # 1.实例化基础模型
+    net = Net()
+    
+    # 2. 加载训练和测试数据
+    train_data = ModelUtils.get_data_loader(True,50)
+    test_data = ModelUtils.get_data_loader(False,50)
+    
+    
+    # 3. 定义模型参数优化器 Adam,lr为参数更新步长(太小会速度慢,太大可能会错过最优参数)
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+    
+    # 4 .训练模型
+    
+    epoch=0 #记录迭代次数
+    true_rate=ModelUtils.evaluate(test_data, net) #计算模型准确率
+    print(f"训练迭代\t{epoch}次\t准确率:{true_rate}") #打印初始迭代次数和准确率
+    #   循环训练
+    while True:
+        epoch += 1
+        for (element, label) in train_data:
+            # 梯度清零:每次执行前向传播和反向传播之前，都需要将模型的梯度清零。否则，PyTorch 会在现有的梯度基础上累加新计算的梯度，影响训练结果。
+            net.zero_grad()  
+
+            # 前向传播会自动调用forward()方法:输入层->隐藏层->输出层,得到预测结果output
+            output = net(element.view(-1, 28*28))
+            
+            # 计算损失:使用负对数似然损失函数(nll_loss)来计算模型的output与label之间的差异（损失值）
+            # 损失函数的值越小，表示模型的预测性能越好。
+            loss = torch.nn.functional.nll_loss(output, label)
+            
+            # 反向传播:通过链式法则计算,指明了如何调整参数以减少损失。
+            loss.backward()
+            
+            # 更新参数:使用Adam优化器更新模型的参数，基于前面计算出的梯度对模型进行调整，从而使损失函数值更小。
+            optimizer.step()
+        
+        # 每个epoch后使用测试集计算准确率
+        true_rate=ModelUtils.evaluate(test_data, net)
+        print(f"训练迭代\t{epoch}次\t准确率:{true_rate}")
+        
+         # 当准确率高于0.975,保存模型的,退出训练
+        if true_rate >= 0.975:
+            
+            print("-"*50)
+            print("模型已达标,最终准确率:",true_rate)
+           
+            nowData=datetime.now().strftime("%Y%m%d%H%M%S")
+            model_name=f"./model/numbermodel_{epoch}_{true_rate}_{nowData}.pth"
+            torch.save(net.state_dict(),model_name)
+            
+            break
+        
